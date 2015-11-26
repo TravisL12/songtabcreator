37bf6e90933469e8b3d3f8f4afcaf6a21b4bfb1b
Schuyler Lawrence <skyman@charter.net>
2015-11-25 17:12:39 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name songtabcreatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the songtabcreatorApp
+ */
+angular.module('songtabcreatorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name songtabcreatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the songtabcreatorApp
+ */
+angular.module('songtabcreatorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

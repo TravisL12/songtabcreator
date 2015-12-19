38f8b3d492ca27d15a43daa3d4b3ee5058854ad1
TravisL12 <travis.lawrence12@gmail.com>
2015-12-19 15:50:55 -0800

    Begin creating the tab library and view

---
+++ app/scripts/controllers/tabs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name songtabcreatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the songtabcreatorApp
+ */
+angular.module('songtabcreatorApp')
+  .controller('TabCtrl', function ($scope, Chords) {
+    $scope.Chords = Chords;
+  });

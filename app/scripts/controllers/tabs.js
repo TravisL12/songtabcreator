3aeaa8d36cdf108065c89a5e44bf256edc488a52
TravisL12 <travis.lawrence12@gmail.com>
2015-12-19 22:09:16 -0800

    Chords working

--- app/scripts/controllers/tabs.js
+++ app/scripts/controllers/tabs.js
@@ -12,6 +12,20 @@ angular.module('songtabcreatorApp')
     $scope.Chords = Chords;
     $scope.columnCount = 110;
     $scope.cells = {};
+
+    $scope.lookupChord = function() {
+      var chord = this.cells['chords'+this.column];
+      var matchedChord = Chords[chord];
+      if (matchedChord !== undefined) {
+        this.cells['e'+this.column] = matchedChord[5];
+        this.cells['b'+this.column] = matchedChord[4];
+        this.cells['g'+this.column] = matchedChord[3];
+        this.cells['d'+this.column] = matchedChord[2];
+        this.cells['a'+this.column] = matchedChord[1];
+        this.cells['E'+this.column] = matchedChord[0];
+      }
+    };
+
     $scope.buildArray = function(num) {
       var a = new Array(num);
       for(var i=1; i<=a.length; i++){ 
